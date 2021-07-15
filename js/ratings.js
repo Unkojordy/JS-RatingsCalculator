65b7b2df006882a17f1f9b14905f0a00c98b7b32
Unkojordy <jordan.eror@gmail.com>
2021-07-15 05:56:09 -1000

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -1,24 +1,22 @@
 const collect_ratings = () => {
-    const ratings = {'count': 0, 'sum': 0, 'average': 0  };
-
+    const ratings = { 'count': 0, 'sum': 0, 'average': 0 };
+  
     let rating = 0;
-
-
-const element = document.querySelectorAll('.rating');
-elements.forEach(element => {
-    rating = parseInt(element.id.replace('star', ' '));
-    ratings.count += parseInt(element.value)
-    ratings.sum += parseInt(element.value) * rating;
-});
-
-if (ratings.count !== 0) {
-    ratings.average = ratings.sum / ratings.count;
-}
-
-return ratings;
-}
-
-document.addEventListener('change', () => {
+    const elements = document.querySelectorAll('.rating');
+    elements.forEach(element => {
+      rating = parseInt(element.id.replace('star', ''));
+      ratings.count += parseInt(element.value);
+      ratings.sum += parseInt(element.value) * rating;
+    });
+  
+    if (ratings.count !== 0) {
+      ratings.average = ratings.sum / ratings.count;
+    }
+  
+    return ratings;
+  };
+  
+  document.addEventListener('change', () => {
     const ratings = collect_ratings();
-    document.querySector('#average').value = ratings.average.toFixed(2);
-});
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+  }); 
